3193a9bdf381ada25c36c385affdf4386a86e908
CrazyDevo <adam@cybertekschool.com>
2022-04-08 14:09:03 -0400

    @wip deleted from CukesRunner

--- src/test/java/demo_sg/CukesRunner.java
+++ src/test/java/demo_sg/CukesRunner.java
@@ -13,6 +13,6 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt",},
         features = "src/test/resources/features",
         dryRun = false,
-        tags = "@wip"
+        tags = ""
 )
 public class CukesRunner {}
